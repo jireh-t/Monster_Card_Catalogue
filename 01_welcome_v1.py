3e07c1d954943dac860891d7e013ab3d093edbf7
jireh-t <98923573+jireh-t@users.noreply.github.com>
2023-05-01 10:05:07 +1200

    Welcome

---
+++ 01_welcome_v1.py
@@ -0,0 +1,14 @@
+"""Version 1 of welcome and menu screen without using easygui """
+
+
+# Print welcome statement
+print("Welcome to Monster Card Catalogue")
+
+# Ask user to enter a number
+choice = input("What would you like to do?\n"
+               "1) Add card\n"
+               "2) Search card\n"
+               "3) Delete card\n"
+               "4) Output card\n"
+               "5) Exit\n"
+               "Enter number here: ")
