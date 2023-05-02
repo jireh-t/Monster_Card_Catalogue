9441d9f276ebc2efa7667e6450591fe30f6c23af
jireh-t <98923573+jireh-t@users.noreply.github.com>
2023-05-02 12:37:54 +1200

    Create 03_add_card_v1_t1.py

---
+++ 03_add_card_v1_t1.py
@@ -0,0 +1,18 @@
+"""Component 2 Add combo version 1
+This code will be trial 1 using an enterbox with multiple fields to get the
+user's combo"""
+
+import easygui
+
+# Stores burger combos in a nested dictionary
+combos = {"Stoneling":
+              {"Strength": 7,
+               "Speed": 1,
+               "Stealth": 25,
+               "Cunning": 15,}
+          }
+
+new_combo = {}
+
+
+
