a07d5d693f4719adcd14740880a237710f83b7de
jireh-t <98923573+jireh-t@users.noreply.github.com>
2023-05-02 12:37:52 +1200

    Create 02_blank_checker_v3_func.py

---
+++ 02_blank_checker_v3_func.py
@@ -0,0 +1,23 @@
+"""Puts code from version 2 into a function"""
+
+import easygui
+
+
+# Function to make sure input is entered and not left blank
+def blank_checker(question, title):
+    error_message = "You must fill out every field"
+
+    # Asks user for input
+    answer = easygui.enterbox(question, title)
+
+    while True:  # Loops until valid input is entered
+        if answer == "":  # Checks if it is blank
+            easygui.msgbox(error_message, "ERROR")  # Display error message
+            answer = easygui.enterbox(question, title)
+        else:
+            return answer
+
+
+# Main Routine
+monster = blank_checker("Enter monster name", "MONSTER")
+easygui.msgbox(f"Monster name: {monster}")
