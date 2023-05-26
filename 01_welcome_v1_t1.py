ffacde6add7be6a90fd2e06ded049e1c5f25afd9
jireh-t <98923573+jireh-t@users.noreply.github.com>
2023-05-27 10:42:00 +1200

    edit

--- 01_welcome_v1_t1.py
+++ 01_welcome_v1_t1.py
@@ -12,3 +12,19 @@ choice = input("What would you like to do?\n"
                "4) Output card\n"
                "5) Exit\n"
                "Enter number here: ")
+
+# Testing the code to make sure it corresponds to the correct option
+if choice == "1":
+    print("Add card")
+
+elif choice == "2":
+    print("Search card")
+
+elif choice == "3":
+    print("Delete card")
+
+elif choice == "4":
+    print("Output card")
+
+elif choice == "5":
+    print("Exit")
